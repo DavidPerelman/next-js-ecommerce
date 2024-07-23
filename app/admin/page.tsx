1ae5760a38d4e48a60b0e123e1de886d5a3e81bd
David Perelman <dperelman3@gmail.com>
2024-07-23 22:07:07 +0300

    admin - page.tsx setup AdminDashboard

---
+++ app/admin/page.tsx
@@ -0,0 +1,20 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
+
+export default function AdminDashboard() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Card>
+        <CardHeader>Sales</CardHeader>
+        <CardDescription>desc</CardDescription>
+        <CardContent>
+          <p>Text</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
